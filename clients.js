2ad82ec7696b9c0eba9eadb378a27d25c5934b6e
Jumphol Tangtrairattanakul <jumphol@crie.co.th>
2013-11-20 18:43:24 +0700

    add file

---
+++ clients.js
@@ -0,0 +1,327 @@
+// a little demo ecosystem that betrays my horrifically incomplete knowledge of plant biology
+
+// the oak object - sucks up water, photosynthesises, grows
+ 
+
+var Officer = Base.extend({  
+
+  constructor: function(Diners, settings) {
+
+    this.Diners = Diners;
+    _.extend(this, settings);
+    
+    this.StartTime = new Date().getTime();
+    this.EndTime = this.StartTime + (this.TimeDuration/2 * 1000); 
+  }
+})
+
+Officer.prototype = { 
+  //state : "idle",
+  Appetize : 5, //max 10
+  Budget : 100,
+  TimeSchedule : 9, //max 10
+  TimeDuration : 60,  
+
+  Job : "officer", 
+  PointOfLike : 5,
+  Comment : "I 'm hungry",
+  Feeling : "normal",
+  Friends : 0,
+  Favorites : ["rice","pepsi"],
+  Hates : ["noodles", "ice_scream"],
+ 
+  FoodBuy : [],
+  DrinkBuy : [],
+  hasAppetize: function() { return this.Appetize > 1; },
+  hasBudget: function() { return this.Budget > 0; },
+  hasTimeSchedule: function() { return this.TimeSchedule > 6; },
+  hasTimeDuration: function() { return this.TimeDuration > 5; },
+  hasFriends: function() { return this.Friends > 0; },
+  isFavorites: function(name) { return _.contains( this.Favorites , name);  },
+  isHates: function(name) { return _.contains( this.Hates , name);  },
+  chooseDiner: function() { 
+    
+    //console.log('this.Diners ', this.Diners );
+    var self = this;
+    var sel = _.sortBy(self.Diners, function(diner){ 
+       
+      //console.log('intersection ', diner.Menus , self.Favorites );
+
+      var matchlikes = _.intersection( diner.Menus , self.Favorites ) || [];
+      var matchExcludeUnlikes = _.difference( matchlikes , self.Hates);
+
+      //console.log('matchlikes ', matchlikes );
+      //console.log('matchExcludeUnlikes ', matchExcludeUnlikes );
+ 
+      var sorted = [];
+      var scroe = 0;
+      if( diner.stock > 0){
+
+        if(matchExcludeUnlikes.length > 0){
+          // score from Taste + Appeal + ... 
+          sorted = _.sortBy(matchExcludeUnlikes, function(match){ return match.Taste + match.Appeal; });
+        }
+        //console.log('sorted ', sorted );
+        if (sorted.length == 0){
+          return scroe;
+        }else{
+
+          for(var i=0; i < sorted.length; i++ ){
+            scroe += (sorted[i].Taste + sorted[i].Appeal); 
+          }
+          scroe /= sorted.length; 
+
+          return scroe; 
+        } 
+      }else{
+        return scroe;
+      }
+    });
+    //console.log('food_sel ', sel );
+ 
+    var food_sel = sel[0] || self.Diners[ Math.floor( Math.random() * (self.Diners.length - 1) ) ]; 
+    this.TargetFood = food_sel;
+    this.TargetFoodPrice = food_sel.Price;
+
+    console.log('this.TargetFood ', this.TargetFood, ' this.TargetFoodPrice ', this.TargetFoodPrice );
+
+    /* -------------------------------------- drink --------------------------------------------- */
+    var drinke_shop = _.filter(this.Diners, function(diner){ 
+      return diner.type == 'drink';
+    });
+    
+    var drink = _.sortBy(drinke_shop, function(diner){ 
+       
+      var matchlikes = _.intersection( diner.Menus , self.Favorites) || [];
+      var matchExcludeUnlikes = _.difference( matchlikes , self.Hates);
+      
+      var sorted = [];
+      var scroe = 0;
+      if( diner.stock > 0){
+
+        if(matchExcludeUnlikes.length > 0){
+          // score from Taste + Appeal + ... 
+          sorted = _.sortBy(matchExcludeUnlikes, function(match){ return match.Taste + match.Appeal; });
+        }
+        //console.log('sorted ', sorted );
+        if (sorted.length == 0){
+          return scroe;
+        }else{
+
+          for(var i=0; i < sorted.length; i++ ){
+            scroe += (sorted[i].Taste + sorted[i].Appeal); 
+          }
+          scroe /= sorted.length; 
+
+          return scroe; 
+        }
+        
+      }else{
+        return scroe;
+      }
+    });
+
+    var drink_sel = drink[0] || drinke_shop[ Math.floor( Math.random() * (drinke_shop.length - 1) ) ]; 
+    this.TargetDrink = drink_sel;
+    this.TargetDrinkPrice = drink_sel.Price;
+
+    console.log('this.TargetDrink ', this.TargetDrink, ' this.TargetDrinkPrice ', this.TargetDrinkPrice );
+
+    return true;  
+  } 
+};
+
+Officer.states = {
+
+  idle: function() { 
+  },
+ 
+  choose: function() {
+    //this.state.identifier = "choose"; 
+    console.log(' ** choose chooseDiner', this.name, this.state.identifier);
+    //this.chooseDiner();
+  },
+
+  canChoose: function() { 
+    console.log(' -- canChoose', this.name, this.hasAppetize() ,  this.state.identifier , this.hasAppetize() && this.state.identifier == "idle");
+    return this.hasAppetize() && this.state.identifier == "idle"; 
+  },
+
+  queue: function() {
+    //this.state.identifier = "queue"; 
+    this.TargetFood.queue.push(this);
+    console.log(' ** queue', this.name, ' length ', this.TargetFood.queue.length);  
+  },
+
+  canQueue: function() { 
+    console.log(' -- canQueue', this.name, ' this.Budget', this.Budget, 'this.TargetFoodPrice',  this.TargetFoodPrice , 'this.Budget > this.TargetFoodPrice ', this.Budget > this.TargetFoodPrice , this.state.identifier);  
+    return this.TargetFoodPrice && this.Appetize < 9 && this.Budget > this.TargetFoodPrice && this.TimeDuration > 0 && this.state.identifier == "choosed"; 
+  },
+
+  buyFood: function() {
+    //this.state.identifier = "buy"; 
+    this.Budget -= this.TargetFoodPrice;
+    this.FoodBuy.push(this.TargetFood);
+
+    console.log(' ** buyFood', this.name, this.TargetFoodPrice, this.Budget );
+    //this.choose();
+  },
+
+  canBuyFood: function() {
+    console.log(' -- canBuyFood', this.name, this.Budget > this.TargetFoodPrice ,this.state.identifier );
+    
+    return this.TargetFoodPrice && this.Budget > this.TargetFoodPrice && this.state.identifier ==  "queue"; 
+  },
+
+  buyDrink: function() {
+    //this.state.identifier = "buy"; 
+    this.Budget -= this.TargetDrinkPrice;
+    this.DrinkBuy.push(this.TargetDrink);
+    console.log(' ** buyDrink', this.name, this.TargetDrinkPrice, this.Budget );
+  }, 
+
+  canBuyDrink: function() {
+    console.log(' -- canBuyDrink', this.name, this.Budget > this.TargetDrinkPrice ,this.state.identifier );
+    
+    return this.TargetDrinkPrice && this.Appetize < 9 && this.Budget > this.TargetDrinkPrice && this.TimeDuration > 0 ;
+  },
+
+  bill: function() { 
+    //bill charge from client.
+    if (this.state.identifier == "buyFood"){
+      this.Budget -= this.TargetFoodPrice;
+    }else if (this.state.identifier == "buyDrink"){
+      this.Budget -= this.TargetDrinkPrice;
+    }  
+    console.log(' ** bill | Budget ', this.name, this.Budget );
+    this.state.identifier = "idle"; 
+  }, // nothing to do - just a state
+
+  canBill: function() { 
+    console.log(' -- canBill', this.name );
+    return this.Budget > 0 && (this.state.identifier == "buyFood" || this.state.identifier == "buyDrink");  
+  },
+  
+  leave: function() {
+    console.log(' ** leave', this.name);
+  }, 
+
+  canLeave: function() {
+    console.log(' -- canLeave', this.name, this.FoodBuy.length , this.DrinkBuy.length );
+    this.CurrentTime = new Date().getTime();
+    if (this.CurrentTime - this.EndTime > 0 ){
+      return true;
+    }
+
+    return this.FoodBuy.length > 0 || this.DrinkBuy.length > 0 || this.TimeDuration < 0 || this.Budget < 10;
+  },
+   
+/*
+  gatherWater: function() {
+    this.water += this.landscape.giveWater();
+  },
+  canGatherWater: function() { return this.landscape.hasWater(); },*/
+};
+
+// the landscape object - rains or shines
+var Diner = Base.extend({  
+
+  constructor: function(settings) {
+    _.extend(this, settings);
+
+    this.StartTime = new Date().getTime();
+    this.EndTime = this.StartTime + (this.TimeDuration/2 * 1000); 
+  }
+});
+
+Diner.prototype = {
+  isOpen: function() { return this.stock > 0; },
+ 
+  type : 'food',
+  _state : "idle",
+  stock : 50,
+  cooking_speed : 0.5,
+  TimeDuration : 6 * 60,
+
+  Price : 35,
+  Taste : 1,
+  Appeal : 1,
+  Menus : ["pat-thai","order"],
+
+  Health : 1,
+  Texture : 1,
+  Brain : 1,
+  Aroma : 1,
+  Appearance : 1,
+  Volume : 1,
+  Rarity : 1,
+  Charisma : 1,
+ 
+  queue : [], 
+  Income : 0
+};
+
+Diner.states = {
+  idle: function() {     
+  },
+
+  fetch: function() { 
+    console.log(' ** fetch', this.name, this.queue.length);
+    this.queue = _.rest(this.queue);  
+  },
+
+  canFetch: function() { 
+    console.log(' -- canFetch', this.name, this.queue.length, this.state.identifier );
+    return this.queue.length > 0 && this.state.identifier == "idle"; 
+  },
+
+  cook: function() { 
+    var self = this;
+    /*
+    console.log(' ---------------** cook', this.name, self.cooking_speed);
+    if(this.cookTime != undefined){
+      clearTimeout(this.cookTime);
+    }
+    this.cookTime = setTimeout(function(){
+       self.cook_done = true; 
+       console.log(' ** cook | cook_done' );
+    }, (1/self.cooking_speed) * 1000 );
+
+    console.log(' ** cook', this.name, self.cooking_speed);*/
+  }, 
+
+  canCook: function() { 
+    console.log(' -- canCook', this.name, this.stock , this.state.identifier );
+    return this.stock > 0 && this.state.identifier == "fetch";  
+  },
+
+  bill: function() { 
+    this.cook_done = false;
+    //bill charge from client.
+    this.Income += this.Price;
+    console.log(' ** bill | Income ', this.name, this.Income, this.Price);
+    this.state.identifier = "idle"; 
+  }, // nothing to do - just a state
+
+  canBill: function() { 
+    console.log(' -- canBill', this.name, this.cook_done );
+    return this.cook_done && this.state.identifier == "cooked"; //Math.random() > 0.1; 
+  },
+
+  close: function() {
+    console.log(' ** leave', this.name);
+  }, 
+
+  canClose: function() {
+    console.log(' -- canLeave', this.name, this.Income , this.stock );
+    if (this.stock <= 0){
+      return true;
+    }
+    this.CurrentTime = new Date().getTime();
+    if (this.CurrentTime - this.EndTime > 0 ){
+      return true;
+    }
+
+    return false;
+  },
+};
---
+++ data.js
@@ -0,0 +1,227 @@
+/* ---------------------  diner  --------------------------*/
+var diner1 = new Diner({
+  name : "ar-harn-tam-sang kao-moo-ob", 
+  stock : 50,
+  cooking_speed : 2,
+  Price : 40,
+  Taste : 5,
+  Appeal : 4,
+  Menus : ["order","kao-moo-ob"],
+});  
+
+var diner2 = new Diner({
+  name : "chai-sii-mee-geaw yen-tafoo tom-yam", 
+  stock : 30,
+  cooking_speed : 2.5,
+  Price : 35,
+  Taste : 3,
+  Appeal : 5,
+  Menus : ["noodle", "yen-tafoo-noodle", "tom-yum-noodle"],
+}); 
+
+var diner3 = new Diner({
+  name : "yum", 
+  stock : 30,
+  cooking_speed : 1.5,
+  Price : 35,
+  Taste : 4,
+  Appeal : 4,
+  Menus : ["yum", "yum-mama", "kao-yum"],
+}); 
+
+var diner4 = new Diner({
+  name : "khuy-teow + khuy-jub", 
+  stock : 20,
+  cooking_speed : 1.5,
+  Price : 35,
+  Taste : 2,
+  Appeal : 2,
+  Menus : ["thai-noodle","khuy-jub"],
+}); 
+
+var diner5 = new Diner({
+  name : "som-tam", 
+  stock : 30,
+  cooking_speed : 3,
+  Price : 40,
+  Taste : 4,
+  Appeal : 5,
+  Menus : ["som-tam","kai-yang"],
+}); 
+
+var diner6 = new Diner({
+  name : "kha-nom-jean + kao-soi", 
+  stock : 30,
+  cooking_speed : 0.8,
+  Price : 35,
+  Taste : 4,
+  Appeal : 2,
+  Menus : ["kha-nom-jean","kao-soi"],
+}); 
+
+var diner7 = new Diner({
+  name : "khuy-teow-nuer", 
+  stock : 30,
+  cooking_speed : 2.8,
+  Price : 40,
+  Taste : 5,
+  Appeal : 5,
+  Menus : ["beef-noodle","duck-noodle"],
+}); 
+
+var diner8 = new Diner({
+  name : "ar-harn-nheuar", 
+  stock : 30,
+  cooking_speed : 1,
+  Price : 45,
+  Taste : 5,
+  Appeal : 5,
+  Menus : ["kao-clook-kapi","kanom-jeen","kao-soi","kao-nam-prik"],
+}); 
+
+var diner9 = new Diner({
+  name : "nam-warn", 
+  type : 'drink',
+  stock : 50,
+  cooking_speed : 0.5,
+  Price : 15,
+  Taste : 4,
+  Appeal : 2,
+  Menus : ["juices","coffee","lum-yai","kra-jeab","oo-leang"],
+}); 
+
+var diner10 = new Diner({
+  name : "pepsi", 
+  type : 'drink',
+  stock : 50,
+  cooking_speed : 0.5,
+  Price : 15,
+  Taste : 4,
+  Appeal : 2,
+  Menus : ["pepsi","water","ma-praw"],
+}); 
+
+var diner11 = new Diner({
+  name : "kao-tom + leard-moo + jok + pat-thai", 
+  stock : 30,
+  cooking_speed : 2 ,
+  Price : 35,
+  Taste : 4,
+  Appeal : 2,
+  Menus : ["tom-leard-moo", "jok", "pat-thai"],
+}); 
+
+var diner12 = new Diner({
+  name : "khuy-teow-namtok", 
+  stock : 30,
+  cooking_speed : 2,
+  Price : 35,
+  Taste : 5,
+  Appeal : 4,
+  Menus : ["pork-noodle", "namtok-noodle", "beef-noodle"],
+}); 
+ 
+var diner13 = new Diner({
+  name : "kao-moo-dang + moo-krob + pat-sii-eaw + raad-nha", 
+  stock : 30,
+  cooking_speed : 1.5,
+  Price : 35,
+  Taste : 4,
+  Appeal : 5,
+  Menus : ["kao-moo-dang", "kao-moo-krob", "pat-sii-eaw", "raad-nha"],
+}); 
+
+var diner13 = new Diner({
+  name : "khuy-teow-tom-yam-moo", 
+  stock : 30,
+  cooking_speed : 2,
+  Price : 35,
+  Taste : 3,
+  Appeal : 4,
+  Menus : ["tom-yam-noodle"],
+}); 
+
+var diner14 = new Diner({
+  name : "ar-harn-pak-tai", 
+  stock : 30,
+  cooking_speed : 1,
+  Price : 40,
+  Taste : 4,
+  Appeal : 5,
+  Menus : ["kao-rad-kang"],
+}); 
+
+var diner15 = new Diner({
+  name : "tam-sang", 
+  stock : 30,
+  cooking_speed : 0.8,
+  Price : 35,
+  Taste : 2,
+  Appeal : 2,
+  Menus : ["order"],
+}); 
+
+var diner16 = new Diner({
+  name : "kao-kha-moo", 
+  stock : 30,
+  cooking_speed : 1,
+  Price : 35,
+  Taste : 4,
+  Appeal : 3,
+  Menus : ["kao-kha-moo"],
+}); 
+
+var diner17 = new Diner({
+  name : "jan-suy", 
+  stock : 30,
+  cooking_speed : 0.8,
+  Price : 40,
+  Taste : 4,
+  Appeal : 4,
+  Menus : ["kao-rad-kang"],
+}); 
+
+var diner18 = new Diner({
+  name : "kao-mun-kai-ton", 
+  stock : 30,
+  cooking_speed : 1,
+  Price : 35,
+  Taste : 4,
+  Appeal : 3,
+  Menus : ["kao-mun-kai"],
+}); 
+
+var diner18 = new Diner({
+  name : "kao-rad-kang-thai", 
+  stock : 30,
+  cooking_speed : 1,
+  Price : 35,
+  Taste : 4,
+  Appeal : 3,
+  Menus : ["kao-rad-kang"],
+}); 
+
+var diner = new Diner({
+  name : "kao-rad-kang-raan-mai", 
+  stock : 30,
+  cooking_speed : 1,
+  Price : 40,
+  Taste : 4,
+  Appeal : 5,
+  Menus : ["kao-rad-kang"],
+}); 
+/* ---------------------  end officer  --------------------------*/
+
+var diners = [diner, diner1,diner2,diner3,diner4,diner5,diner6,diner7,diner8,diner9,diner10,diner11,diner12,diner13,diner14,diner15,diner16,diner17,diner18];
+
+/* ---------------------  officer  --------------------------*/
+
+var officer = new Officer(diners, {
+  name : "tester", 
+  Appetize : 5, //max 10
+  Budget : 100,
+  TimeSchedule : 9, //max 10
+  TimeDuration : 60,  
+  Favorites : ["kao-rad-kang","pepsi"],
+  Hates : ["beef-noodle", "juices"],
+});  
