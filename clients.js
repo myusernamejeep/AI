2bebdf2e8d81610bac281d55ce1f256700cce180
Jumphol Tangtrairattanakul <jumphol@crie.co.th>
2013-11-22 14:49:22 +0700

    ปรับการ log ใน state ให้ สื่อความหมาย
    รองรับการ implment ui ต่อไป

--- clients.js
+++ clients.js
@@ -113,7 +113,7 @@ Officer.prototype = {
     this.TargetFood = food_sel;
     this.TargetFoodPrice = food_sel.Price;
 
-    console.log('this.TargetFood ', this.TargetFood, ' this.TargetFoodPrice ', this.TargetFoodPrice );
+    console.log('I choose to order ' , this.TargetFood , '  Price ', this.TargetFoodPrice );
 
     /* -------------------------------------- drink --------------------------------------------- */
     var drinke_shop = _.filter(this.Diners, function(diner){ 
@@ -155,12 +155,11 @@ Officer.prototype = {
     this.TargetDrink = drink_sel;
     this.TargetDrinkPrice = drink_sel.Price;
 
-    console.log('this.TargetDrink ', this.TargetDrink, ' this.TargetDrinkPrice ', this.TargetDrinkPrice );
+    console.log('I choose to order Drink ', this.TargetDrink, ' Price ', this.TargetDrinkPrice );
 
     return true;  
   } ,
 
-  states:{ LOOKING:0,BUYING:1},   
   directions:{ DOWN:0, LEFT:1, RIGHT:2, UP:3,},
   
   sourceX: 0,
@@ -190,6 +189,7 @@ Officer.prototype = {
   numberOfFrames: 3, //0-3
   currentFrame: 0,
   timer: 0,
+  states:{ INIT:0,GOTOSHOP:1, QUEUETOBUY:2, ISYOURQUEUE:3, WAITFORCOOK:4, GETORDERFOOD:5, GOTO_TABLE_EATZONE:6, EATFOOD:7, RESET:8, GO_BACK:9},   
     
   init:function(){
     this.sourceXPos = this.sourceX;
@@ -213,51 +213,54 @@ Officer.prototype = {
   },
   buy:function(){
     if(this.Budget <= 0){
-      this.buyState = 8;
-     
+      this.buyState = this.states.GO_BACK;
     }    
     switch(this.buyState){
-        case 0:
+        case this.states.INIT:
           this.buyInit();         
           break;
           
-        case 1:
+        case this.states.GOTOSHOP:
           this.goToShop();
           break;  
           
-        case 2:
+        case this.states.QUEUETOBUY:
           this.queueToBuy();
           break;      
           
-        case 3:
+        case this.states.ISYOURQUEUE:
           this.isYourQueue();
           break;
 
-        case 4:
-          this.buyOrderFood();
+        case this.states.WAITFORCOOK:
+          this.waitForCook();
+          break;
+
+        case this.states.GETORDERFOOD:
+          this.getOrderFood();
           break;
           
-        case 5:
+        case this.states.GOTO_TABLE_EATZONE:
           this.goToEatTable();
           break;
 
-        case 6:
+        case this.states.EATFOOD:
           this.eatFood();
           break;
             
-        case 7:
+        case this.states.RESET:
           this.buyState = 0;
           break;   
 
-        case 8:
+        case this.states.GO_BACK:
           this.goBack();
           break;
                                  
       }                                       
   },
   buyInit:function(){
-    if(this.Budget <= 0){
-      this.buyState = 8;
+    if(this.Budget <= 0 || this.Appetize < 1){
+      this.buyState = this.states.GO_BACK;
       return false;
     }
     this.angle = Math.random() * 2 * Math.PI; 
@@ -267,75 +270,88 @@ Officer.prototype = {
     this.targetY = this.TargetFood.posY;
     this.setDirection( this.targetX - this.x, this.targetY - this.y );    
               
-    this.buyState = 1;
+    this.buyState = this.states.GOTOSHOP;
   },
   goToShop:function(){    
    
     this.moveTo(this.targetX, this.targetY );
-      
-      if (this.x == this.targetX && this.y == this.targetY) {
-        this.currentFrame = 0;
-        this.changeSprite();
-        this.buyState = 2;     
-        console.log(' --** goToShop complete ', this.name , this.TargetFood);         
-      }            
+ 
+    if (this.x == this.targetX && this.y == this.targetY) {
+      this.currentFrame = 0;
+      this.changeSprite();
+      this.buyState = this.states.QUEUETOBUY;     
+      console.log(' --** I walk to '+ this.name + ' complete. I think I wil buy one of ' +  this.TargetFood.name + ' which cost ' + this.TargetFood.Price);         
+    }         
   },
   queueToBuy:function(){ 
     if (_.indexOf(this.TargetFood.queue, this) == -1 && !this.add_to_queue ){
       this.TargetFood.queue.push(this); 
       this.add_to_queue =  true;
-      console.log(' --** add to queue , TargetFood.queue size ', this.TargetFood.queue.length );   
+      console.log(' --** I go to queue at row of this shop , row queue size is ', this.TargetFood.queue.length );   
     }   
-    //this.stand();
+    this.stand();
 
     var num_queue = this.TargetFood.queue.length;
     var your_index = _.indexOf(this.TargetFood.queue, this);
     var y = this.targetY + (your_index*this.height);
     this.moveTo(this.targetX, y );
-    console.log(' --** your_index ', your_index,  y );   
-    /*if( !(gameTimer % 600) ){
-      this.angle *= -1;               
-      this.setDirection( this.XPos - this.x, this.YPos - this.y );  
-      //this.lookState++;  
-      console.log(' --** queueToBuy complete ', this.name );   
-    }   */  
-         
+    console.log(' --** my queue is #' + your_index,  y );   
+ 
   },
   isYourQueue:function(){
-    console.log(' --** isYourQueue ', this.name );
+    console.log(' --** I is my turn , my queue ' );
+    console.log(' >> I want to order 1 of ' + this.TargetFood.name + ', which cost : ' + this.TargetFood.Price );  
   },
-  buyOrderFood:function(){
+  waitForCook:function(){
+    console.log(" --** I 'm waiting for my order ... ", this.name );
+  },
+  getOrderFood:function(){
     this.stand();
     if (this.TargetFood.type == "food"){
       this.Budget -= this.TargetFoodPrice;
     }else if (this.TargetFood.type == "drink"){
       this.Budget -= this.TargetDrinkPrice;
     }  
-    console.log(' --** bill | Budget ', this.name, this.Budget );
-    this.buyState = 5;     
+    console.log(' --** I bill for '+ this.name + '. My budget is ' + this.Budget );
+    this.buyState = this.states.GOTO_TABLE_EATZONE;     
+
+    var your_index = _.indexOf(this.TargetFood.queue, this);
+    this.eatZoneX = 500;
+    this.eatZoneY = 500 + ( your_index * this.height * Math.random() )
         
   },
   goToEatTable:function(){
-    var your_index = _.indexOf(this.TargetFood.queue, this);
-    this.moveTo(500, 500 + (your_index*this.height*Math.random() ) );
+    this.moveTo(this.eatZoneX, this.eatZoneY);
     
-    if( !(gameTimer % 600) ){
-       this.setDirection( this.XPos - this.x, this.YPos - this.y );  
-       this.buyState++;   
+    if (this.x == this.eatZoneX && this.y == this.eatZoneY) {  
+      console.log(" --** I 'm at the eat zone, sit down on mychair to eatfood. ", this.name );
+      this.setDirection( this.XPos - this.x, this.YPos - this.y );  
+      this.buyState = this.states.EATFOOD; 
+
+      this.eatTimeOut = new Date().getTime() + (10 * 1000);
+      
     } 
   },
   eatFood:function(){
-    console.log(' --** eatFood ', this.name );
+    console.log(" --** I 'm eating food ", this.name );
     if( !(gameTimer % 600) ){
-       this.setDirection( this.XPos - this.x, this.YPos - this.y );  
-       this.buyState++;   
+      console.log(" --** do eating anim ... ", this.name );
+      this.setDirection( this.XPos - this.x, this.YPos - this.y );        
     } 
+
+    this.now = new Date().getTime()
+    if (this.now - this.eatTimeOut > 0 ){
+        // eat done
+        console.log(" --** I eaten my food. ", this.name );   
+        this.Appetize -= Math.floor(Math.random()*4) ;
+        this.buyState = this.states.RESET; 
+    }  
   },
   goBack:function(){
     this.moveTo(-128, -128);
-    
     if( !(gameTimer % 600) ){
-       this.setDirection( this.XPos - this.x, this.YPos - this.y );  
+      console.log(" --** I 'm done. I will go back to my work now. ", this.name );    
+      this.setDirection( this.XPos - this.x, this.YPos - this.y );  
     } 
   },
   look:function(){
@@ -415,25 +431,31 @@ Officer.prototype = {
     var magnitude = Math.sqrt((vx*vx)+(vy*vy));
         
     vx = vx/magnitude;
-      vy = vy/magnitude;
+    vy = vy/magnitude;
       
-      this.stepX = vx * this.speed;
-      this.stepY = vy * this.speed;   
-                
+    this.stepX = vx * this.speed;
+    this.stepY = vy * this.speed;   
+    
+    if (this.stepX){
       this.x += this.stepX;
+    }          
+      
+    if (this.stepY){
       this.y += this.stepY;
+    }          
       
-      if( !(gameTimer % 10) ){
-        this.updateSpriteAnimation();         
-      }
-      
-      if ((vx > 0 && this.x + this.speed >= targetX) || (vx < 0 && this.x - this.speed <= targetX)) {
-          this.x = targetX;
-      }
-      
-      if ((vy > 0 && this.y + this.speed >= targetY) || (vy < 0 && this.y - this.speed <= targetY)) {
-        this.y = targetY;
-      }
+    
+    if( !(gameTimer % 10) ){
+      this.updateSpriteAnimation();         
+    }
+    
+    if ((vx > 0 && this.x + this.speed >= targetX) || (vx < 0 && this.x - this.speed <= targetX) && targetX ) {
+        this.x = targetX;
+    }
+    
+    if ((vy > 0 && this.y + this.speed >= targetY) || (vy < 0 && this.y - this.speed <= targetY) && targetY ) {
+      this.y = targetY;
+    }
         
   },
   
@@ -445,7 +467,9 @@ Officer.prototype = {
   },
   
   changeSprite:function(){
+ 
     this.sourceX = this.currentFrame * this.sourceWidth + this.sourceXPos;
+     
     this.sourceY = this.direction * this.sourceHeight + this.sourceYPos;  
   },
     
@@ -549,28 +573,15 @@ Officer.states = {
     }
 
     return this.FoodBuy.length > 0 || this.DrinkBuy.length > 0 || this.TimeDuration < 0 || this.Budget < 10;
-  },
-   
-/*
-  gatherWater: function() {
-    this.water += this.landscape.giveWater();
-  },
-  canGatherWater: function() { return this.landscape.hasWater(); },*/
+  } 
 };
-
-// the landscape object - rains or shines
+ 
 var Diner = Base.extend({  
-
   constructor: function(settings) {
     _.extend(this, settings);
-
     var self = this;
     this.image = new Image();
-    //this.image.addEventListener("load", this.updateLoop , false);
     this.image.addEventListener("load", $.proxy( this.updateLoop, self )  , false);
-    
-    //console.log(' ------- constructor ', this.cooking_speed);
-    
     this.StartTime = new Date().getTime();
     this.EndTime = this.StartTime + (this.TimeDuration/2 * 1000); 
   } 
@@ -645,7 +656,7 @@ Diner.prototype = {
   },
   idle: function() {   
     if( this.stock <= 0 ){
-      this.dinerState = 6;
+      this.dinerState = this.states.CLOSE;
       return false;
     }
     //console.log(' --** idle ', this.name ); 
@@ -655,17 +666,17 @@ Diner.prototype = {
     //console.log(' --** fetch queue ', this.queue   ); 
     this.current_queue = _.first(this.queue);  
     if (this.current_queue != undefined && this.queue.length > 0 ){
-      console.log(' --** fetch this.current_queue ', this.name, this.current_queue, this.queue   ); 
+      console.log(' --** I fetch client order from queue ', this.name, this.current_queue, this.queue   ); 
       this.queue = _.rest(this.queue);  
-      this.dinerState = 2;
-      this.current_queue.buyState = 3;
+      this.dinerState = this.states.COOK;
+      this.current_queue.buyState = this.states.ISYOURQUEUE;
       //console.log(' --** fetch ', this.current_queue , this.queue.length ); */
     }
     
   },
   cook: function() {   
     if( this.stock <= 0 ){
-      this.dinerState = 6;
+      this.dinerState = this.states.CLOSE;
       return false;
     }
 
@@ -673,12 +684,12 @@ Diner.prototype = {
       return false;
     }
     this.stock -= 1;
-    console.log(' --** cook ', this.name, this.cooking_speed, this.stock);
+    console.log(" --** I 'm cooking for your order ", this.name, this.cooking_speed, this.stock);
     var self = this;
     this.cookTime = setTimeout(function(){
        self.cook_done = true; 
-       console.log(' ** cook | cook_done' );
-       self.dinerState = 3; 
+       console.log(" ** I 's  done! ");
+       self.dinerState = this.states.COOKED; 
 
         if(this.cookTime != undefined){
           clearTimeout(this.cookTime);
@@ -687,88 +698,19 @@ Diner.prototype = {
 
   },
   cooked: function() {   
-    this.dinerState = 4;
+    this.dinerState = this.states.BILL;
     this.current_queue.buyState = 4;
-    console.log(' --** cooked ', this.current_queue ); 
-    
+    console.log(' --** We cooked!', this.current_queue );   
   },
   bill: function() {   
     this.cook_done = false;
     //bill charge from client.
     this.Income += this.Price;
-    console.log(' ** bill | Income ', this.name, this.Income, this.Price);
-    this.dinerState = 0;
+    console.log(' ** We will get bill and receive income amount '+ this.Price + '. For all income total are ' + this.Income );
+    this.dinerState = this.states.IDLE;
   },
   close: function() {   
-    //this.TargetFood.queue.push(this); 
-    //this.dinerState = 0;
+    console.log(' ** We will close my shop now.', this.name );
   },
 };
-
-Diner.states = {
-  idle: function() {   
-  },
-
-  fetch: function() { 
-    console.log(' ** fetch', this.name, this.queue.length);
-    this.queue = _.rest(this.queue);  
-  },
-
-  canFetch: function() { 
-    console.log(' -- canFetch', this.name, this.queue.length, this.state.identifier );
-    return this.queue.length > 0 && this.state.identifier == "idle"; 
-  },
-
-  cooked: function() { 
-  },
-
-  canCooked: function() { 
-    console.log(' -- canCooked', this.name,  this.state.identifier );
-    return this.stock > 0 && this.state.identifier == "cook";  
-  },
-
-  cook: function() { 
-    var self = this;
-    /*
-    console.log(' ---------------** cook', this.name, self.cooking_speed);
-    if(this.cookTime != undefined){
-      clearTimeout(this.cookTime);
-    }
-    this.cookTime = setTimeout(function(){
-       self.cook_done = true; 
-       console.log(' ** cook | cook_done' );
-    }, (1/self.cooking_speed) * 1000 );
-
-    console.log(' ** cook', this.name, self.cooking_speed);*/
-  }, 
-
-  canCook: function() { 
-    console.log(' -- canCook', this.name, this.stock , this.state.identifier );
-    return this.stock > 0 && this.state.identifier == "fetch";  
-  },
-
-  bill: function() {   
-  }, // nothing to do - just a state
-
-  canBill: function() { 
-    console.log(' -- canBill', this.name, this.cook_done );
-    return this.cook_done && this.state.identifier == "cooked"; //Math.random() > 0.1; 
-  },
-
-  close: function() {
-    console.log(' ** leave', this.name);
-  }, 
-
-  canClose: function() {
-    console.log(' -- canLeave', this.name, this.Income , this.stock );
-    if (this.stock <= 0){
-      return true;
-    }
-    this.CurrentTime = new Date().getTime();
-    if (this.CurrentTime - this.EndTime > 0 ){
-      return true;
-    }
-
-    return false;
-  },
-};
+ 
